4d9d1d9396b47e52ac54c2dc6fd8d2ca905499c7
jakeaaronb <47617763+jakeaaronb@users.noreply.github.com>
2022-03-23 13:02:57 -0700

    Add files via upload

---
+++ pizzaOrdered.js
@@ -0,0 +1,15 @@
+<html>
+   <head>      
+      <script type = "text/javascript">
+         
+            function pizzaOrdered() {
+               alert("Pizza Ordered")
+            }
+        
+      </script>     
+   </head>
+   
+   <body>
+      <input type = "button" onclick = "pizzaOrdered()" value = "Pizza Ordered" />
+   </body>  
+</html>
